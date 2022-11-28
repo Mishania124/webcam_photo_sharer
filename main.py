229c007ab23b7825a81474d0e1592a108386ef87
Mishania124 <morozovmike124@gmail.com>
2022-11-28 16:30:25 +0100

    add

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
-    def search_image(self):
+    def get_image_link(self):
         query = self.manager.current_screen.ids.user_query.text
         page = wikipedia.page(query)
         image_link = page.images[0]
-        req = requests.get(image_link)
+        return image_link
+
+    def download_image(self):
+        req = requests.get(self.get_image_link())
         imagepath = 'files/image.jpg'
         with open(imagepath, 'wb') as file:
             file.write(req.content)
-        self.manager.current_screen.ids.img.source = imagepath
+        return imagepath
+
+    def set_image(self):
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 
 class RootWidget(ScreenManager):
